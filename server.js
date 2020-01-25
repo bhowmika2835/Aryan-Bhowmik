2b727993b08f14ece4f64382173416ba924a3a2b
Glitch (hello-express) <none>
2020-01-25 20:43:04 +0000

    🎀💠 Checkpoint ./views/index.html:10677500/247 ./server.js:10677500/10 ./views/form.html:10677634/801

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/submit", function(request, response) {
+app.get("/form", function(request, response) {
   response.sendFile(__dirname + "/views/form.html");
 });
 
