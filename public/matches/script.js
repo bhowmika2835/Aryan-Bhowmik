d81f0f2731f7b522a4a1ca48680acff1f40acfa3
Glitch (marbled-flower) <none>
2020-01-26 09:45:33 +0000

    🚜🐴 Checkpoint ./public/matches/script.js:10677500/185 ./words.json:10677500/32 ./views/matches/index.html:10677500/2 ./views/our_purpose/index.html:10677500/3

--- public/matches/script.js
+++ public/matches/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       console.log(data);
       for(var i = 0; i < JSON.parse(data).animals.length; i++){
         console.log(JSON.parse(data).animals[i])
-        $(JSON.parse(data).animals[i]).Text
+        $(JSON.parse(data).animals[i]).text.html
       }
     }
   });
