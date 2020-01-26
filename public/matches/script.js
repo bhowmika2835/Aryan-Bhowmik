95a2124019ab12afa425b2b3697ad3604100acb2
Glitch (marbled-flower) <none>
2020-01-26 09:21:33 +0000

    👐🌽 Checkpoint ./public/matches/script.js:10677634/383 ./server.js:10677634/4

--- public/matches/script.js
+++ public/matches/script.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
     url: "/getmatches",
     success: function(data) {
       console.log(data);
+      for(var i = 0; i < JSON.parse(data).animals.length; i++){
+        console.log(JSON.parse(data).animals[i])
+        $(JSON.parse(data).animals[i]).Text
+      }
     }
   });
 });
